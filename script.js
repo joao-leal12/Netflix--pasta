04ec6bebc16f1186de9ae448443723fa7271b283
Joao <joaovictorleal080415@gmail.com>
2022-03-09 13:53:59 -0300

    comit

---
+++ script.js
@@ -0,0 +1,143 @@
+
+function interacao(none, block) {
+
+    document.getElementById('fechar').style.display = block
+    document.getElementById('abrir').style.display = none
+
+
+
+}
+
+
+
+function fechar(block , none) {
+
+
+        document.getElementById('fechar').style.display = none
+        document.getElementById('abrir').style.display = block 
+
+
+
+
+}
+//logica da segunda parte 
+
+function interacaoDois(none, block) {
+
+    document.getElementById('close').style.display = block
+    document.getElementById('segundo').style.display = none
+    
+
+
+}
+
+
+
+function fecharDois(block , none) {
+
+
+    document.getElementById('close').style.display = none
+    document.getElementById('segundo').style.display = block 
+
+
+
+
+}
+
+
+
+//logica tres 
+function interacaoT(none, block) {
+
+    document.getElementById('closeP').style.display = block
+    document.getElementById('terceiro').style.display = none
+    
+
+
+}
+
+
+function fechartres(block , none) {
+
+
+    document.getElementById('closeP').style.display = none
+    document.getElementById('terceiro').style.display = block 
+
+
+
+
+}
+
+//logica quatro 
+
+
+function interacaoQ(none, block) {
+
+    document.getElementById('closeQ').style.display = block
+    document.getElementById('Quarto').style.display = none
+    
+
+
+}
+
+
+function fecharquatro(block , none) {
+
+
+    document.getElementById('closeQ').style.display = none
+    document.getElementById('Quarto').style.display = block 
+
+
+
+
+}
+
+
+//logica cinto 
+
+
+function interacaoC(none, block) {
+
+    document.getElementById('closeC').style.display = block
+    document.getElementById('Cinco').style.display = none
+    
+
+
+}
+
+
+function fecharcinco(block , none) {
+
+
+    document.getElementById('closeC').style.display = none
+    document.getElementById('Cinco').style.display = block 
+
+
+
+
+}
+
+//logica seis 
+
+
+
+function interacaoS(none, block) {
+
+    document.getElementById('closeS').style.display = block
+    document.getElementById('seis').style.display = none
+    
+
+
+}
+
+
+function fecharseis(block , none) {
+
+
+    document.getElementById('closeS').style.display = none
+    document.getElementById('seis').style.display = block 
+
+
+
+
+}
